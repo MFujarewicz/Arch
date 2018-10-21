0752c58d900c75705e1bf3b0b02f9dc0e21c146f
MFujarewicz <michal.fujarewicz@gmail.com>
2018-10-21 20:53:31 +0000

    added kolorki source

---
+++ kolorki.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  for (int i = 0; i<16; i++){
+    cout << "\e[38;5;" << i << "m" << "HelloWorld" << endl;
+  }
+  cout <<"\e[0m" << endl; //Resets ASCII to default
+  return 0;
+}
