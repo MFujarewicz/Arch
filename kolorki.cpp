d3dd806224f242cd92213d3db49a2d82d461ea6f
MFujarewicz <michal.fujarewicz@gmail.com>
2018-10-21 20:58:41 +0000

    changed greeting language to english

--- kolorki.cpp
+++ kolorki.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
   for (int i = 0; i<16; i++){
-    cout << "\e[38;5;" << i << "m" << "WitajŚwiecie" << endl;
+    cout << "\e[38;5;" << i << "m" << "Hello World!" << endl;
   }
   cout <<"\e[0m" << endl; //Resets ASCII to default
   return 0;
